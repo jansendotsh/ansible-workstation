ce6fb7b78b6fd1afada17d8e4019ba2a2d340b03
garrettjj <garrett@jansen.sh>
2019-03-22 23:44:57 -0400

    Adding Visual Studio Code, AppImage & Gnome themes

---
+++ scripts/appimage.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create directory
+mkdir -p $HOME/Applications
+
+# StandardNotes
+curl -s https://api.github.com/repos/standardnotes/desktop/releases/latest |
+    grep "x86_64.AppImage" |
+    cut -d '"' -f 4 |
+    wget -O $HOME/Applications/StandardNotes.AppImage -qi -
+
+chmod +x $HOME/Applications/StandardNotes.AppImage
+
+# Caprine
+curl -s https://api.github.com/repos/sindresorhus/caprine/releases/latest |
+    grep "x86_64.AppImage" |
+    cut -d '"' -f 4 |
+    wget -O $HOME/Applications/Caprine.AppImage -qi -
+
+chmod +x $HOME/Applications/Caprine.AppImage

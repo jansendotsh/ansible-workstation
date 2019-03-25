b5c9583cb85d4b88ae02bccbfcf0c1af16fb78b9
garrettjj <garrett@jansen.sh>
2019-03-25 01:03:55 -0400

    Add Google Chrome and PolarBookshelf, refactoring

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -18,3 +18,11 @@ curl -s https://api.github.com/repos/sindresorhus/caprine/releases/latest |
     wget -O $HOME/Applications/Caprine.AppImage -qi -
 
 chmod +x $HOME/Applications/Caprine.AppImage
+
+# PolarBookshelf
+curl -s https://api.github.com/repos/burtonator/polar-bookshelf/releases/latest |
+    grep "x86_64.AppImage" |
+    cut -d '"' -f 4 |
+    wget -O $HOME/Applications/PolarBookshelf.AppImage -qi -
+
+chmod +x $HOME/Applications/PolarBookshelf.AppImage

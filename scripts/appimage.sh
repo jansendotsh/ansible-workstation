3a9642ef6afebfe522ef98967c5c114517dfdf36
Garrett Jansen <garrett@jansen.sh>
2019-09-30 08:27:11 -0400

    Fixing Caprine issue

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -13,7 +13,7 @@ chmod +x $HOME/Applications/StandardNotes.AppImage
 
 # Caprine
 curl -s https://api.github.com/repos/sindresorhus/caprine/releases/latest |
-    grep "x86_64.AppImage" |
+    grep "AppImage" |
     cut -d '"' -f 4 |
     wget -O $HOME/Applications/Caprine.AppImage -qi -
 

6f3c01fed86c26793cb40511ec550e24419f3ca8
Garrett Jansen <garrett@jansen.sh>
2021-04-20 19:24:24 -0700

    Only SN now
    Only StandardNotes now.

--- scripts/appimage.sh
+++ scripts/appimage.sh
@@ -11,19 +11,3 @@ curl -s https://api.github.com/repos/standardnotes/desktop/releases/latest |
     wget -O $HOME/Applications/StandardNotes.AppImage -qi -
 
 chmod +x $HOME/Applications/StandardNotes.AppImage
-
-# Caprine
-curl -s https://api.github.com/repos/sindresorhus/caprine/releases/latest |
-    grep "AppImage" |
-    cut -d '"' -f 4 |
-    wget -O $HOME/Applications/Caprine.AppImage -qi -
-
-chmod +x $HOME/Applications/Caprine.AppImage
-
-# PolarBookshelf
-curl -s https://api.github.com/repos/burtonator/polar-bookshelf/releases/latest |
-    grep "x86_64.AppImage" |
-    cut -d '"' -f 4 |
-    wget -O $HOME/Applications/PolarBookshelf.AppImage -qi -
-
-chmod +x $HOME/Applications/PolarBookshelf.AppImage
